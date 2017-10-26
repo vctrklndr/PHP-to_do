0c5cd100da75389ababcf0ed5ddc31e9783e237b
Datamaskinen <Datamaskinen@Datamaskinen-II.local>
2017-10-26 20:56:02 +0200

    Changed tables to divs

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
     <footer>
         <div class="footer">
-            Username: vctrklndr<br />
-            <a href="https://github.com/vctrklndr">https://github.com/vctrklndr</a>
+            vctrklndr / <a href="https://github.com/vctrklndr">https://github.com/vctrklndr</a>
         </div> 
     </footer>
 </body>
--- index.php
+++ index.php
@@ -6,11 +6,34 @@
     require 'complete.php';
     require 'fetch.php';
 ?>
-    <header>
-        <h1>I have so much stuffz to do!</h1>
-    </header>
+    <!-- <header>
+        <h1>I have so much stuff to-do list!</h1>
+    </header> -->
 
     <main>
+        <header>
+            <h1>Add to to-do list:</h1>
+        </header>
+        <form method="POST">
+            <div class="add_wrapper">
+                <div class="entry">To do:</div>
+                <div class="entry">
+                    <input type="text" name="title" required />
+                </div>
+                <div class="entry">Author:</div>
+                <div class="entry">
+                    <input type="text" name="createdBy" required />
+                </div>
+            </div> <!-- add wrapper container -->
+        
+        <div class="button_container">
+            <input type="submit" value="Add" />
+            <div class="reload">
+                <a href="index.php"><i class="fa fa-refresh" aria-hidden="true"></i></a>
+            </div>
+        </div><!-- button container --> 
+        </form>
+
         <div class="success_message">
             <?php
                 if (isset($_SESSION['success'])){
@@ -21,78 +44,54 @@
         
         <h2>Stuff to do:</h2>
 
-        <table>
-            <tbody>
-                <?php
-                    foreach ($to_do as $to_do_list):
-                        if ($to_do_list['completed'] == 0){ 
-                ?>
-                <tr>
-                    <td class="title"><?= $to_do_list['title']; ?></td>
-                    <td class="author"><?= $to_do_list['createdBy']; ?></td>
-                    <td>
-                        <form method="POST" action="delete.php">
-                            <input type="hidden" name="delete" value="<?= $to_do_list['id'] ?>" />
-                            <button type= "submit" value="Delete">
-                                <i class="fa fa-trash-o" aria-hidden="true"></i>
-                            </button>
-                        </form>
-                    </td>
-                    <td>
-                        <form method="POST" action="complete.php">
-                            <input type="hidden" name="completed" value="<?= $to_do_list['id'] ?>" />
-                            <button type= "submit" value="Done">
-                                <i class="fa fa-check" aria-hidden="true"></i>
-                            </button>
-                        </form>
-                    </td>
-                </tr>
-                <?php } endforeach; ?>
-            </tbody>
-        </table>
-        
-        <div class="add_wrapper">
-            <h2>Add to to-do list:</h2>
-            
-            <form method="POST">
-                <table>
-                    <tr>
-                        <td class="entry">To do:</td>
-                        <td><input type="text" name="title" required /></td>
-                    </tr>
-                    <tr>
-                        <td class="entry">Author:</td>
-                        <td><input type="text" name="createdBy" required /></td>
-                    </tr>
-                </table>
-                <div class="button_container">
-                    <input type="submit" value="Add" />
-                    <div class="reload">
-                        <a href="index.php"><i class="fa fa-refresh" aria-hidden="true"></i></a>
-                    </div>
-                </div><!-- button container -->
-            </form>
-        </div> <!-- add wrapper container -->
-
-        <h2>Completed:</h2>
-        <table>
-            <tr>
+        <div class="to_do_wrapper">
             <?php
                 foreach ($to_do as $to_do_list):
-                    if ($to_do_list['completed'] == 1){ ?>
-                <td class="title"><?= $to_do_list['title']; ?></td>
-                <td class="author"><?= $to_do_list['createdBy']; ?></td>
-                <td>
+                    if ($to_do_list['completed'] == 0){ 
+            ?>
+            <div class="to_do_container">
+                <div class="title"><?= $to_do_list['title']; ?></div>
+                <div class="author"><?= $to_do_list['createdBy']; ?></div>
+                <div>
                     <form method="POST" action="delete.php">
                         <input type="hidden" name="delete" value="<?= $to_do_list['id'] ?>" />
                         <button type= "submit" value="Delete">
-                                <i class="fa fa-trash-o" aria-hidden="true"></i>
-                            </button>
+                            <i class="fa fa-trash-o" aria-hidden="true"></i>
+                        </button>
                     </form>
-                </td>     
-            </tr>
+                </div>
+                <div>
+                    <form method="POST" action="complete.php">
+                        <input type="hidden" name="completed" value="<?= $to_do_list['id'] ?>" />
+                        <button type= "submit" value="Done">
+                            <i class="fa fa-check" aria-hidden="true"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <?php } endforeach; ?>
+        </div>
+
+        <h2>Completed:</h2>
+
+        <div class="complete_wrapper">
+        <?php
+            foreach ($to_do as $to_do_list):
+                if ($to_do_list['completed'] == 1){ ?>
+            <div class="complete_container">
+                <div class="title"><?= $to_do_list['title']; ?></div>
+                <div class="author"><?= $to_do_list['createdBy']; ?></div>
+                <div>
+                    <form method="POST" action="delete.php">
+                        <input type="hidden" name="delete" value="<?= $to_do_list['id'] ?>" />
+                        <button type= "submit" value="Delete">
+                            <i class="fa fa-trash-o" aria-hidden="true"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
             <?php } endforeach;  ?>
-        </table>
+        </div>
     </main>
 <?php
     require 'footer.php';
