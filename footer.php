7ca4700902f6ae1175577b361b119af9410bd707
vctrklndr <32761959+vctrklndr@users.noreply.github.com>
2017-10-27 12:44:20 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     <footer>
         <div class="footer">
-            vctrklndr @ <a href="https://github.com/vctrklndr" target="_blank">GitHub</a>
+            vctrklndr @ <a href="https://github.com/vctrklndr/to_do_list" target="_blank">GitHub</a>
         </div> 
     </footer>
 </body>
